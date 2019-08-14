ad17f22f25d5b671f45181bf8025b320ab7b9bdf
michaelbrazier <brazier.6@osu.edu>
2019-08-13 21:57:09 -0400

    add CWLetterBox

---
+++ src/components/CWLetterBox.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Coords from './../types/Coords';
+import './CWLetterBox.css';
+
+export interface CWLetterBoxProps {
+  character: string;
+  selected: boolean;
+  validated: boolean;
+  coords: Coords;
+  onMouseDown: (coords: Coords) => void;
+  onMouseEnter: (coords: Coords) => void;
+}
+
+interface CWLetterBoxState {
+  selected: boolean;
+}
+
+const CWLetterBox: React.FC<CWLetterBoxProps> = props => {
+  return (
+    <div
+      className={`CWLetterBox ${props.selected ? 'selected' : ''}
+				${props.validated ? 'validated' : ''}`}
+      onMouseDown={() => {
+        props.onMouseDown(props.coords);
+      }}
+      onMouseEnter={() => {
+        props.onMouseEnter(props.coords);
+      }}
+    >
+      <div className="CWLetterBoxChar">
+        <div>{props.character}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CWLetterBox;
