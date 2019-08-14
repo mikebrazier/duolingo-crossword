6ce02c486e591bf4548101b04fdfd9a01dd5d06b
michaelbrazier <brazier.6@osu.edu>
2019-08-13 22:01:45 -0400

    add static GameAnswerIndicator for footer view

--- src/components/GameAnswerIndicator.tsx
+++ src/components/GameAnswerIndicator.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
-
+import './GameAnswerIndicator.css';
 const GameAnswerIndicatorStyle = {};
 
 function GameAnswerIndicator() {
   return (
-    <div className="GameAnswerIndicator" style={GameAnswerIndicatorStyle}></div>
+    <div className="GameAnswerIndicator" style={GameAnswerIndicatorStyle}>
+      <div className="GameAnswerCircle">
+        <span className="GameAnswerCircleCheck" />
+      </div>
+      <div className="GameAnswerIndicatorContent">
+        <div className="GameAnswerCorrectMessage">
+          <h2>You are correct </h2>
+        </div>
+      </div>
+      <div className="GameContinueButtonWrapper">
+        <button className="GameContinueButton">Continue</button>
+      </div>
+    </div>
   );
 }
 
