f793d833496c0628c2f971197b02b45e2992e16d
michaelbrazier <brazier.6@osu.edu>
2019-08-18 22:24:56 -0400

    add styling & continue/check functionality to GameAnswerIndicator

--- src/components/GameAnswerIndicator.tsx
+++ src/components/GameAnswerIndicator.tsx
@@ -1,23 +1,61 @@
 import * as React from 'react';
 import './GameAnswerIndicator.css';
-const GameAnswerIndicatorStyle = {};
 
-function GameAnswerIndicator() {
-  return (
-    <div className="GameAnswerIndicator" style={GameAnswerIndicatorStyle}>
-      <div className="GameAnswerCircle">
-        <span className="GameAnswerCircleCheck" />
-      </div>
-      <div className="GameAnswerIndicatorContent">
-        <div className="GameAnswerCorrectMessage">
-          <h2>You are correct </h2>
+export interface GameAnswerIndicatorProps {
+  buttonEnabled: boolean;
+  showAnswer: boolean;
+  answerCorrect: boolean;
+  onCheckAnswer: () => void;
+  onContinue: () => void;
+}
+
+interface GameAnswerIndicatorState {}
+
+class GameAnswerIndicator extends React.Component<
+  GameAnswerIndicatorProps,
+  GameAnswerIndicatorState
+> {
+  render() {
+    let answerCorrect = this.props.answerCorrect;
+    return (
+      <div
+        className={`GameAnswerIndicatorWrapper 
+          ${answerCorrect ? 'answerCorrect' : 'answerWrong'}`}
+      >
+        <div className="GameAnswerIndicator">
+          <div
+            className={`GameAnswerMessage
+         ${this.props.showAnswer ? 'showAnswer' : ''}`}
+          >
+            <div className="GameAnswerCircle">
+              <span className="GameAnswerCircleCheck" />
+            </div>
+            <div className="GameAnswerIndicatorContent">
+              <div
+                className={`GameAnswerCorrectMessage
+            ${answerCorrect ? 'answerCorrect' : 'answerWrong'}`}
+              >
+                <h2>
+                  {answerCorrect ? 'Good find!' : "Oops!  That's inncorrect."}
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="GameContinueButtonWrapper">
+            <button
+              className={`GameContinueButton
+                ${this.props.buttonEnabled ? 'enabled' : ''}
+            ${this.props.showAnswer ? 'showAnswer' : ''}
+            ${answerCorrect ? 'answerCorrect' : 'answerWrong'}`}
+              onClick={this.props.onCheckAnswer}
+            >
+              Continue
+            </button>
+          </div>
         </div>
       </div>
-      <div className="GameContinueButtonWrapper">
-        <button className="GameContinueButton">Continue</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default GameAnswerIndicator;
