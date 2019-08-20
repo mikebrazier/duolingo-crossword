c2c4ce56bcf3bb2c118d1ff6dfd912883278feb5
michaelbrazier <brazier.6@osu.edu>
2019-08-20 11:31:29 -0400

    add getLanguageString function

--- src/types/CWLang.tsx
+++ src/types/CWLang.tsx
@@ -2,3 +2,13 @@
 export const EN_LANG = 'en';
 export const ES_LANG = 'es';
 export type CWLang = typeof EN_LANG | typeof ES_LANG;
+
+export function getLanguageString(cwLang: CWLang) {
+  switch (cwLang) {
+    case EN_LANG:
+      return 'English';
+    case ES_LANG:
+      return 'Spanish';
+  }
+  return '';
+}
