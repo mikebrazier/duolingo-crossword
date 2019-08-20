dfbd2b062a2a3c7d81cff6d446d72a74b9f6685e
michaelbrazier <brazier.6@osu.edu>
2019-08-20 11:31:14 -0400

    add getCWLetterFromGridPoint function to CharacterGrid

--- src/types/CharacterGrid.tsx
+++ src/types/CharacterGrid.tsx
@@ -1,30 +1,34 @@
 import Coords from './Coords';
-
-export interface CharacterGridPoint {
-  character: string;
-  coords: Coords;
-  validated: boolean;
-  selected: boolean;
-  index: number;
-}
+import { CWLetter } from './CWWord';
 
 export class CharacterGrid {
-  grid: Array<Array<CharacterGridPoint>>;
+  grid: Array<Array<CWLetter>>;
   constructor(cg: Array<Array<string>>) {
-    this.grid = new Array<Array<CharacterGridPoint>>();
+    this.grid = new Array<Array<CWLetter>>();
     let index = 0;
     for (let i = 0; i < cg.length; ++i) {
       this.grid.push([]);
       for (let j = 0; j < cg[i].length; ++j) {
         this.grid[i].push({
-          character: cg[i][j],
-          coords: { x: j, y: i },
-          validated: false,
-          selected: false,
-          index: index
+          c: cg[i][j],
+          coord: { x: j, y: i }
         });
         ++index;
       }
     }
+  }
+
+  getCWLetterFromGridPoint(coord: Coords): CWLetter | null {
+    for (let i = 0; i < this.grid.length; ++i) {
+      for (let j = 0; j < this.grid[i].length; ++j) {
+        if (
+          this.grid[i][j].coord.x === coord.x &&
+          this.grid[i][j].coord.y === coord.y
+        )
+          return this.grid[i][j];
+      }
+    }
+
+    return null;
   }
 }
