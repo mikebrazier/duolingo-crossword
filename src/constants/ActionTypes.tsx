b55ae5ec712549b03f8dcedfa282609cf328bfd5
michaelbrazier <brazier.6@osu.edu>
2019-08-20 20:13:11 -0400

    add action types, additional reducers

--- src/constants/ActionTypes.tsx
+++ src/constants/ActionTypes.tsx
@@ -1,8 +1,12 @@
 import { CWWord } from './../types/CWWord';
+import { CWGameData } from './../types/CWGame';
+import { AppState } from './../types/AppState';
 export const WORD_SELECTION = 'WORD_SELECTION';
 export const CHECK_SELECTION = 'CHECK_SELECTION';
 export const CONTINUE_GAME = 'CONTINUE_GAME';
 export const GAME_RESET = 'GAME_RESET';
+export const RECEIVE_GAME_DATA = 'RECEIVE_GAME_DATA';
+export const LOAD_APP_STATE = 'LOAD_APP_STATE';
 
 export interface WordSelectionAction {
   type: typeof WORD_SELECTION;
@@ -21,8 +25,20 @@ export interface GameResetAction {
   type: typeof GAME_RESET;
 }
 
+export interface ReceiveGameDataAction {
+  type: typeof RECEIVE_GAME_DATA;
+  payload: Array<CWGameData>;
+}
+
+export interface LoadAppStateAction {
+  type: typeof LOAD_APP_STATE;
+  payload: AppState;
+}
+
 export type ActionTypes =
   | WordSelectionAction
   | CheckSelectionAction
   | ContinueAction
-  | GameResetAction;
+  | GameResetAction
+  | ReceiveGameDataAction
+  | LoadAppStateAction;
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -12,17 +12,31 @@ import {
   WORD_SELECTION,
   CHECK_SELECTION,
   CONTINUE_GAME,
-  GAME_RESET
+  GAME_RESET,
+  RECEIVE_GAME_DATA,
+  LOAD_APP_STATE
 } from './../constants/ActionTypes';
 import { AppState, initialState } from './../types/AppState';
+import { saveState, loadState } from './../types/LocalStorage';
+
+function saveAppState(appState: AppState) {
+  let newAppState: AppState = cloneDeep(appState);
+
+  newAppState.games.forEach(game => {
+    let newState: CWGame.CWGameState = CWGame.newGameState();
+    newState.foundWords = game.state.foundWords;
+  });
+
+  let result = saveState(newAppState);
+}
 
 export function CWAppReducer(
   state = initialState,
   action: ActionTypes
 ): AppState {
-  let newGameState = cloneDeep(state.games[state.gameIndex].state);
   switch (action.type) {
     case WORD_SELECTION: {
+      let newGameState = cloneDeep(state.games[state.gameIndex].state);
       CWGame.setSelectedWord(newGameState, action.payload);
       return {
         games: [
@@ -37,10 +51,12 @@ export function CWAppReducer(
       };
     }
     case CHECK_SELECTION: {
+      let newGameState = cloneDeep(state.games[state.gameIndex].state);
       CWGame.checkSelectedWord(
         state.games[state.gameIndex].gameData,
         newGameState
       );
+
       return {
         games: [
           ...state.games.slice(0, state.gameIndex),
@@ -54,23 +70,24 @@ export function CWAppReducer(
       };
     }
     case CONTINUE_GAME: {
+      let newGameState = cloneDeep(state.games[state.gameIndex].state);
       //always clear the selected word on continue
       CWGame.clearSelectedWord(newGameState);
-
-      //advance to next game once all words found
-      let gameIndex = state.gameIndex;
+      //advance to next game if all words found
+      let newGameIndex = state.gameIndex;
       if (
         CWGame.getWordsRemaining(
           state.games[state.gameIndex].gameData,
-          newGameState
+          state.games[state.gameIndex].state
         ) == 0
       ) {
-        //if not last game
+        //if not last game, increment index
         if (state.gameIndex != state.games.length - 1) {
-          ++gameIndex;
+          ++newGameIndex;
         }
       }
-      return {
+      //define the new app state
+      let newAppState: AppState = {
         games: [
           ...state.games.slice(0, state.gameIndex),
           {
@@ -79,16 +96,39 @@ export function CWAppReducer(
           },
           ...state.games.slice(state.gameIndex + 1)
         ],
-        gameIndex: gameIndex
+        gameIndex: newGameIndex
       };
+      //save to local storage
+      saveAppState(newAppState);
+
+      return newAppState;
     }
+    //on reset
     case GAME_RESET: {
+      //deep copy all the games
       let newGames = cloneDeep(state.games);
+      //clear the state for each game
       newGames.forEach(game => (game.state = CWGame.newGameState()));
-      return {
+      //create new app state with updates games array
+      let newAppState: AppState = {
         games: newGames,
         gameIndex: 0
       };
+      //save to local storage
+      saveAppState(newAppState);
+      //return
+      return newAppState;
+    }
+    case RECEIVE_GAME_DATA: {
+      return {
+        games: action.payload.map(gD => {
+          return CWGame.makeCWGame(gD);
+        }),
+        gameIndex: 0
+      };
+    }
+    case LOAD_APP_STATE: {
+      return action.payload;
     }
     default:
       return state;
