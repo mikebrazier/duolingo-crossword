522a946b506084737395adb514e89960ab25a483
michaelbrazier <brazier.6@osu.edu>
2019-08-21 12:39:43 -0400

    add documentation

--- src/api/duolingoAPI.tsx
+++ src/api/duolingoAPI.tsx
@@ -2,8 +2,8 @@
  *  @brief Function for fetching data from Duolingo API & parsing into GameData JSON array
  *
  *  During development, the Duolingo API is accessed via a proxy set in package.json
- *  This proxy points all requests to the baseURL: http://s3.amazonaws.com/duolingo-data/s3/js
- *  The full URL where game data is hosted is: http://s3.amazonaws.com/duolingo-data/s3/js/find_challenges.txt
+ *  This proxy points all requests to the baseURL: http://s3.amazonaws.com/duolingo-data/s3/js2
+ *  The full URL where game data is hosted is: http://s3.amazonaws.com/duolingo-data/s3/js2/find_challenges.txt
  *
  *  @author Mike Brazier
  */
@@ -14,6 +14,7 @@ import { CWRawGameData, CWGameData, parseCWRawGameData } from '../types/CWGame';
  * Constants
  ***************************************/
 
+// const baseURL = "http://exampleBaseURL.com/api";
 const challengesEndpoint = '/find_challenges.txt';
 
 /***************************************
